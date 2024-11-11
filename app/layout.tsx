b14a5be39c29757fd4ddbc684d95adb47aa66c0e
dolnikmate <mate.dolnik@cometstudio.io>
2024-11-11 16:09:39 +0100

    feat: app modification to only feature a single simple chat page chore: metadata, icon changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,9 @@ import { assistantId } from "./assistant-config";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Assistants API Quickstart",
-  description: "A quickstart template using the Assistants API with OpenAI",
+  title: "Assistant test chat",
   icons: {
-    icon: "/openai.svg",
+    icon: "/comet.svg",
   },
 };
 
@@ -17,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {assistantId ? children : <Warnings />}
-        <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
+        <img className="logo" src="/comet.svg" alt="OpenAI Logo" width={"32px"} height={"32px"}/>
       </body>
     </html>
   );
